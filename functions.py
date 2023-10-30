036a830871f605b9865f5fe2008bc4f9acf0d86d
Dennis <148398668+Sikkepit@users.noreply.github.com>
2023-10-30 19:46:01 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -29,11 +29,12 @@ def validate_input(accepted_values, prompt):
     while not validated:
         input_to_be_validated = input(prompt + "\n" + str(accepted_values) + "> ")
         for value in accepted_values:
-            if value == input_to_be_validated:
+            if value.lower() == input_to_be_validated.lower():
+                input_to_be_validated = value
                 validated = True
                 break
         if not validated:
-            print("The input is in invalid.")
+            print("The input is invalid.")
     return input_to_be_validated
 
 
@@ -54,18 +55,20 @@ def validate_time_slot(list_with_time_slots):
     while not validated:
         time_slot = input("Please type the number of your preferred time slot: ")
         if not time_slot.isnumeric():
-            print("The number is invalid")
+            print("The number is invalid.")
         elif int(time_slot) > len(list_with_time_slots):
-            print("The time slot doesn't exist")
+            print("The time slot doesn't exist.")
         else:
             return time_slot
 
 
 def make_appointment(barber_list, treatment_name_list, treatment_detail_list, free_slots):
-    barber = validate_input(barber_list, "Who would you like to help you?")
+    print("It's good to see you! You can use this app to schedule an appointment for your next haircut.")
+    barber = validate_input(barber_list, "Who is your barber of choice?")
     treatment = validate_input(treatment_name_list, "What kind of treatment would you like?")
     duration = get_treatment_duration(treatment, treatment_detail_list)
     choose_a_time = list_free_slots(free_slots, duration)
+    print("These are the available timeslots you can choose from:")
     for time_slot in choose_a_time:
         print(f"{time_slot}")
     chosen_time_slot = validate_time_slot(choose_a_time)
@@ -79,7 +82,7 @@ def make_appointment(barber_list, treatment_name_list, treatment_detail_list, fr
     return appointment
 
 
-# Check which time-slots are available looking at planned shifts and current appointments
+# Dividing a shift into 15 minute time blocks
 def get_free_slots(shifts, appointments, barber):
     available_slots = []
     for shift in shifts:
@@ -94,6 +97,7 @@ def get_free_slots(shifts, appointments, barber):
                 available_slots.append([i, block, "Available"])
                 i += 1
 
+# Checking which time blocks are already taken by other appointments
     for appointment in appointments:
         blocked_slots = 0
         for slot in available_slots:
@@ -106,7 +110,7 @@ def get_free_slots(shifts, appointments, barber):
     return available_slots
 
 
-# Looking at which time-slots are available considering the duration of the treatment of the customer
+# Looking at which overlapping time blocks are unavailable considering the duration of appointments
 def list_free_slots(free_slots, duration):
     slots = int((duration / 15))
     only_free_slots = []
@@ -114,8 +118,8 @@ def list_free_slots(free_slots, duration):
         if slot[2] != 'Unavailable':
             only_free_slots.append(slot)
     last_slot = len(only_free_slots)
-    i = 0
-    x = 0
+    i = 0  # index for all the slots, available and unavailable
+    x = 0  # index for available slots
     list_with_slots = []
     while i < last_slot - slots:
         if only_free_slots[i][1] == only_free_slots[i + slots][1] - timedelta(minutes=duration):
